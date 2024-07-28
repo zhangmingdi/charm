24ed96398276d67b655f7ae334ae5a297c096714
mingdi <3201896772@qq.com>
2024-07-28 22:57:42 +0800

    导航页搞定

---
+++ apps/builder/src/views/data-source-view/DataSourcePage.vue
@@ -0,0 +1,11 @@
+<script  setup lang="ts">
+
+
+</script>
+
+<template>
+    <div class="homePageWrapper">
+        我是自
+    </div>
+</template>
+
---
+++ apps/builder/src/views/home/HomePage.vue
@@ -0,0 +1,19 @@
+<script  setup lang="ts">
+import HomeNav from './parts/HomeNav.vue';
+import { RouterView } from 'vue-router'
+
+
+</script>
+
+<template>
+      <div class="homePageWrapper">
+        <header>
+            <HomeNav />
+        </header>
+        <main>
+            <RouterView/>
+        </main>
+    </div>
+</template>
+
+<style src="./style.css" scoped></style>
