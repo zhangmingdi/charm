9ff9e8c14b46c95bcbae1fad8f5f13fd9c2a6275
mingdi <3201896772@qq.com>
2025-03-24 22:58:12 +0800

    feat: add trpc

--- apps/my-next/src/app/[local]/(home)/page.tsx
+++ apps/my-next/src/app/[local]/(home)/page.tsx
@@ -1,12 +1,19 @@
-import Image from "next/image";
+import { trpc } from "@/trpc/server";
 import { useTranslations } from "next-intl";
+import { Suspense } from "react";
+
+const DataComponent = async () => {
+  const data = await trpc.hello({ text: "hello1" });
+  return <div>I am going to load video {data.greeting}</div>;
+};
 
 export default function Home() {
   const t = useTranslations("HomePage");
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      I am going to load video
+      <Suspense fallback={<div>Loading...</div>}>
+        <DataComponent />
+      </Suspense>
     </div>
   );
 }
